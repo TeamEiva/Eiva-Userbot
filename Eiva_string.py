367da178399005f3609d3786d8d94c9b0401ed29
SHIVANSH <75574324+Error-Shivansh@users.noreply.github.com>
2021-07-04 13:55:27 +0530

    EIVA BOT

---
+++ Eiva_string.py
@@ -0,0 +1,20 @@
+import os
+os.system("pip install telethon")
+import telethon
+from telethon.sessions import StringSession
+from telethon.sync import TelegramClient
+
+
+okvai = input("Enter 69 to continue: ")
+if okvai == "69":
+    print("Please go to my.telegram.org and get your API Id and API Hash to proceed.")
+    APP_ID = int(input("Enter APP ID here: "))
+    API_HASH = input("Enter API HASH here: ")
+
+    with TelegramClient(StringSession(), APP_ID, API_HASH) as client:
+        print(client.session.save())
+        client.send_message("me", client.session.save())
+        client.send_message("me", "Above is your #EivaBOT_SESSION \nPaste this string in Heroku Var.\n\n[Team EivaBot](t.me/Eiva50_bot)")
+
+else:
+    print("Bhag jaa bhosdike")
