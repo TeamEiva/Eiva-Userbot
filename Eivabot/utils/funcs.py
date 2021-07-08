656e0b70838913ef2f88f078a30aabee5a008741
SHIVANSH <75574324+errorshivansh@users.noreply.github.com>
2021-07-08 07:50:40 +0530

    Update funcs.py

--- Eivabot/utils/funcs.py
+++ Eivabot/utils/funcs.py
@@ -33,8 +33,8 @@ async def is_admin(client, chat_id, user_id):
     if not str(chat_id).startswith("-100"):
         return False
     try:
-        Error-Shivansh = await client(GetParticipantRequest(channel=chat_id, user_id=user_id))
-        chat_participant = Error-Shivansh.participant
+        errorshivansh = await client(GetParticipantRequest(channel=chat_id, user_id=user_id))
+        chat_participant = errorshivansh.participant
         if isinstance(
             chat_participant, (ChannelParticipantCreator, ChannelParticipantAdmin)
         ):
