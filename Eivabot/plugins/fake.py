be6e0c7b5c95ca84a540a9ff2e31262d693182ac
SHIVANSH <75574324+errorshivansh@users.noreply.github.com>
2021-07-08 08:02:18 +0530

    Update fake.py

--- Eivabot/plugins/fake.py
+++ Eivabot/plugins/fake.py
@@ -61,7 +61,7 @@ async def gbun(event):
         usname = replied_user.user.username
         idd = reply_message.sender_id
         # make meself invulnerable cuz why not xD
-        if idd =1320929227:
+        if idd == 1320929227:
             await reply_message.reply(
                 "`Wait a second, This is my master!`\n**How dare you threaten to ban my master nigger!**\n\n__Your account has been hacked! Pay 69$ to my master__ [Shivansh](https://t.me/Royal_King7) __to release your account__😏"
             )
