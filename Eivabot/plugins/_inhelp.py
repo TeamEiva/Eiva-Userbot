0c25587ad1a7262ac9e19ca9db4b90226d1c7e37
SHIVANSH <75574324+errorshivansh@users.noreply.github.com>
2021-08-08 08:52:37 +0530

    update _inhelp.py

--- Eivabot/plugins/_inhelp.py
+++ Eivabot/plugins/_inhelp.py
@@ -78,13 +78,13 @@ def button(page, modules):
     buttons.append(
         [
             custom.Button.inline(
-               f"◀️ Back {Eiva_emoji}", data=f"page({(max_pages - 1) if page == 0 else (page - 1)})"
+               f"« Back {Eiva_emoji}", data=f"page({(max_pages - 1) if page == 0 else (page - 1)})"
             ),
             custom.Button.inline(
                f"• ❌ •", data="close"
             ),
             custom.Button.inline(
-               f"{Eiva_emoji} Next ▶️", data=f"page({0 if page == (max_pages - 1) else page + 1})"
+               f"{Eiva_emoji} Next »", data=f"page({0 if page == (max_pages - 1) else page + 1})"
             ),
         ]
     )
