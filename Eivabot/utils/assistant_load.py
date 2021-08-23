d601a18a9be5a09ab15fc633bfca48d95def6942
Developer <72003733+InternetAmethyst@users.noreply.github.com>
2021-08-23 21:28:51 +0530

    assistant manager

---
+++ Eivabot/utils/assistant_load.py
@@ -0,0 +1,48 @@
+import asyncio
+import datetime
+import importlib
+import inspect
+import logging
+import math
+import os
+import re
+import sys
+import time
+import traceback
+from pathlib import Path
+from time import gmtime, strftime
+
+from telethon import events
+from telethon.tl.functions.channels import GetParticipantRequest
+from telethon.tl.types import ChannelParticipantAdmin, ChannelParticipantCreator
+
+from .. import *
+from ..helpers import *
+from ..config import Config
+
+
+def start_assistant(shortname):
+    if shortname.startswith("__"):
+        pass
+    elif shortname.endswith("_"):
+        import importlib
+        import sys
+        from pathlib import Path
+
+        path = Path(f"Eivabot/assistant/{shortname}.py")
+        name = "Eivabot.assistant.{}".format(shortname)
+        spec = importlib.util.spec_from_file_location(name, path)
+        mod = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(mod)
+    else:
+        import importlib
+        import sys
+        from pathlib import Path
+
+        path = Path(f"Eivabot/assistant/{shortname}.py")
+        name = "Eivabot.assistant.{}".format(shortname)
+        spec = importlib.util.spec_from_file_location(name, path)
+        mod = importlib.util.module_from_spec(spec)
+        mod.tgbot = bot.tgbot
+        spec.loader.exec_module(mod)
+        sys.modules["assistant" + shortname] = mod
